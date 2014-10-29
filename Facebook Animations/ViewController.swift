fddbe7efd07e19dd3c204b5da12d7ea0a0098888
atwoodamanda <atwooda86@gmail.com>
2014-10-29 06:21:27 -0700

    Initial Commit

---
+++ Facebook Animations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Facebook Animations
+//
+//  Created by Amanda Atwood on 10/29/14.
+//  Copyright (c) 2014 Amanda Atwood. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

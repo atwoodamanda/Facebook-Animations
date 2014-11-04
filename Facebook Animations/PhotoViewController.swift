b155d78c55de843f81bbc344be93f37b4f5beee0
atwoodamanda <atwooda86@gmail.com>
2014-11-03 19:48:54 -0800

    background becomes transparent upon scroll.

--- Facebook Animations/PhotoViewController.swift
+++ Facebook Animations/PhotoViewController.swift
@@ -8,14 +8,21 @@
 
 import UIKit
 
-class PhotoViewController: UIViewController {
+class PhotoViewController: UIViewController, UIScrollViewDelegate {
     
     @IBOutlet weak var imageView: UIImageView!
     var image: UIImage!
 
+    @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var photoActions: UIImageView!
+    @IBOutlet weak var doneButton: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        scrollView.delegate = self
         imageView.image = image
+        scrollView.contentSize = CGSize(width: 320, height: 620)
 
         // Do any additional setup after loading the view.
     }
@@ -24,11 +31,20 @@ class PhotoViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
 
     @IBAction func onDoneButton(sender: AnyObject) {
         dismissViewControllerAnimated(true, completion: nil)
     }
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        var number = 1 - abs(scrollView.contentOffset.y/100)
+        println(number)
+        photoActions.alpha = number
+        doneButton.alpha = 0.0001
+        backgroundView.alpha = number
+        if
+    }
+    
 
 }
